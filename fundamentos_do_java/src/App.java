157decf26bb3813d2d324f2ae0cd22867e5a4a55
memlith <iamcaiosimonassi@gmail.com>
2025-06-17 17:50:16 -0300

    adiciona hello world a fundamentos

---
+++ fundamentos_do_java/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Configurando a JDK na versao " + args[0]);
+        System.out.println("Programando em " + args[1]);
+    }
+}
