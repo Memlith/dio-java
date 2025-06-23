70cb3e52785ba913781b53d3933d5a4f648d369d
memlith <iamcaiosimonassi@gmail.com>
2025-06-23 16:26:33 -0300

    adiciona atividade de debugging e excecoes

---
+++ debugging-e-excecoes/src/br/com/dio/exception/CustomException.java
@@ -0,0 +1,9 @@
+package br.com.dio.exception;
+
+public class CustomException extends RuntimeException {
+
+  public CustomException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
+
+}
---
+++ debugging-e-excecoes/src/br/com/dio/exception/EmptyStorageException.java
@@ -0,0 +1,7 @@
+package br.com.dio.exception;
+
+public class EmptyStorageException extends Exception {
+  public EmptyStorageException(final String message) {
+    super(message);
+  }
+}
